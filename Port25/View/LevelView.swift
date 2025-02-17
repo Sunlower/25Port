04b18e2618bdf3dcf9b92900aff727b0bc9f9944
Sunflower <ie_maria2000@outlook.com>
2025-02-17 11:35:32 -0300

    New Sprites

---
+++ Port25/View/LevelView.swift
@@ -0,0 +1,13 @@
+//
+//  Level.swift
+//  Port25
+//
+//  Created by Ieda Xavier on 07/02/25.
+//
+
+.onChange(of: message) {
+    whenAnimationEnd.toggle()
+    typeWriter1 {
+        whenAnimationEnd.toggle()
+    }
+}
